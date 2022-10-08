35f593f303d18d8966d7dce29a80740c0b52e0a1
longhrzdev <shiroxhorizon@gmail.com>
2022-10-08 15:36:15 +0700

    home slider, content

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,69 @@ searchIcon.onclick = () => {
     searchForm.classList.toggle('active')
     window.addEventListener('click', handleCloseSearchForm)
 }
+
+// home slider
+const prevSliderBtn = document.querySelector('.home_slider_prev')
+const nextSliderBtn = document.querySelector('.home_slider_next')
+const sliderStage = document.querySelector('.slider_stage')
+const sliderItems = document.querySelectorAll('.slider_item')
+const sliderCustomDots = document.querySelectorAll('.slider_custom_dot')
+
+// set stage width 
+const setWidthSlider = () => {
+    const stageWidth = window.innerWidth * sliderItems.length
+    const sliderWidth = window.innerWidth
+    sliderStage.style.width = `${stageWidth}px`
+
+    sliderItems.forEach(e => {
+        e.style.width = `${sliderWidth}px`
+    })
+
+    return [stageWidth, sliderWidth]
+}
+setWidthSlider()
+// set width when resize
+window.addEventListener('resize', setWidthSlider)
+
+// get 'next' or 'prev' string param return new active index
+const setActiveSlider = (control) => {
+    let activeIndex
+
+    // get prev active index and remove all active
+    sliderItems.forEach((e, index) => {
+        if (e.classList.contains('active')) {
+            activeIndex = index
+        }
+        e.classList.remove('active')
+    })
+
+    sliderCustomDots.forEach(e => e.classList.remove('active'))
+
+    // set active index when next btn or prev btn click
+    if (control === 'next') {
+        activeIndex = activeIndex < sliderItems.length - 1 ? activeIndex + 1 : 0
+    } else if (control === 'prev') {
+        activeIndex = activeIndex > 0 ? activeIndex - 1 : sliderItems.length - 1
+    }
+
+    // set active
+    sliderItems[activeIndex].classList.add('active')
+    sliderCustomDots[activeIndex].classList.add('active')
+    return activeIndex
+}
+
+const setSliderTransform = (control) => {
+    const [stageWidth, sliderWidth] = setWidthSlider()
+    const tranX = sliderWidth * setActiveSlider(control)
+    sliderStage.style = `width: ${stageWidth}px ;transform: translateX(-${tranX}px); transition: all 0.8s ease;`
+}
+
+// next btn
+nextSliderBtn.onclick = () => {
+    setSliderTransform('next')
+}
+
+// prev btn
+prevSliderBtn.onclick = () => {
+    setSliderTransform('prev')
+}
